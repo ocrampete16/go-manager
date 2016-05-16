d4587038a7485ac9799328841e526f981daff6cd
Marco Petersen <m@rcopetersen.com>
2016-05-17 00:39:41 +0200

    Implement everything except auto-switching :)

--- go-manager.sh
+++ go-manager.sh
@@ -1,3 +1,4 @@
+GO_MANAGER_DIR="$(cd "$(dirname $0)" && pwd)"
 HELP_TEXT="Go Manager
 A minimalist approach to managing multiple Go installations. Inspired by chruby and ruby-install.
 
@@ -20,49 +21,107 @@ Commands:
 Visit https://github.com/ocrampete16/go-manager#readme for more details.
 "
 
-function _go_manager_install()
-{
-  echo "Installing..."
-}
-
-function _go_manager_list()
-{
-  echo "Listing..."
-}
-
-function _go_manager_use()
-{
-  if [[ $1 == "--default" ]]; then
-    echo $2 > $HOME/.go-version
-    export GOROOT=$HOME/.go-manager/gos/$2
-    export PATH=$PATH:$GOROOT/bin
-    export GOPATH=$HOME/.go-manager/workspaces/$2
-  else
-    export GOROOT=$HOME/.go-manager/gos/$1
-    export PATH=$PATH:$GOROOT/bin
-    export GOPATH=$HOME/.go-manager/workspaces/$1
+install_go_version() {
+  local version="$1"
+  if [[ "$version" == "latest" ]]; then
+    version="$(
+      git ls-remote --tags https://go.googlesource.com/go \
+        | awk -F '/' '{ print $NF }' \
+        | grep --invert-match --regexp='beta' --regexp='rc' --regexp='release' --regexp='weekly' \
+        | awk -F 'o' '{ print $NF }' \
+        | tail --lines=1
+    )"
   fi
+
+  local installation_dir=$GO_MANAGER_DIR/gos/$version
+
+  if [[ "$(uname)" == "Darwin" ]]; then
+    local operating_system="darwin"
+    local file_extension="pkg"
+  elif [[ "$(uname)" == "Linux" ]]; then
+    local operating_system="linux"
+    local file_extension="tar.gz"
+  fi
+
+  if [[ "$(uname -m)" == "x86_64" ]]; then
+    local bit_architecture="amd64"
+  elif [[ "$(uname -m)" == "i386" ]]; then
+    local bit_architecture="386"
+  fi
+
+  local archive_url="https://storage.googleapis.com/golang/go$version.$operating_system-$bit_architecture.$file_extension"
+
+  mkdir $installation_dir
+  wget --output-document=- $archive_url \
+    | tar --gzip --extract --file=- --directory=$installation_dir --strip-components=1
 }
 
-function _go_manager_uninstall()
-{
-  rm -rf $HOME/.go-manager/gos/$1
+list_available_go_versions() {
+  echo "Available Go versions:"
+  git ls-remote --tags https://go.googlesource.com/go \
+    | awk -F '/' '{ print $NF }' \
+    | grep --invert-match --regexp='beta' --regexp='rc' --regexp='release' --regexp='weekly' \
+    | awk -F 'o' '{ print $NF }' \
+    | awk '{ print "  " $0 }'
 }
 
-function go-manager()
-{
-  case $1 in
+list_installed_go_versions() {
+  echo "Installed Go versions:"
+  find $GO_MANAGER_DIR/gos -mindepth 1 -maxdepth 1 -type d \
+    | awk -F '/' '{ print $NF }' \
+    | awk '{ print "  " $0 }'
+}
+
+use_go_version() {
+  local version="$1"
+  export GOROOT=$GO_MANAGER_DIR/gos/$version
+  export PATH=$PATH:$GOROOT/bin
+}
+
+make_go_version_default() {
+  local version="$1"
+  echo $version > $HOME/.go-version
+}
+
+uninstall_go_version() {
+  local version="$1"
+  rm -rf $GO_MANAGER_DIR/gos/$version
+  unset GOROOT
+}
+
+go-manager() {
+  local command="$1"
+
+  case $command in
     "install")
-      _go_manager_install $2
+      local version="$2"
+      install_go_version $version
       ;;
     "list")
-      _go_manager_list $2
+      if [[ "$2" == "available" ]]; then
+        list_available_go_versions
+      else
+        list_installed_go_versions
+      fi
       ;;
     "use")
-      _go_manager_use $2 $3
+      if [[ "$2" == "--default" ]]; then
+        local make_default_version=true
+        local version="$3"
+      else
+        local make_default_version=false
+        local version="$2"
+      fi
+
+      use_go_version $version
+
+      if [[ "$make_default_version" == true ]]; then
+        make-go-version-default $version
+      fi
       ;;
     "uninstall")
-      _go_manager_uninstall $2
+      local version="$2"
+      uninstall_go_version $version
       ;;
     *)
       echo $HELP_TEXT
